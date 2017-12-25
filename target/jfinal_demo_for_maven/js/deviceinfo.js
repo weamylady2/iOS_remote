88ef22481a4c794fc6200e700effd3b051da0769
weamylady2 <312743168@qq.com>
2017-12-25 11:07:08 +0800

    add .gitignore

--- target/jfinal_demo_for_maven/js/deviceinfo.js
+++ target/jfinal_demo_for_maven/js/deviceinfo.js
@@ -1,46 +1,24 @@
 
-var deviceDetail = new Vue({
-    el: "#deviceinfo",
-    data: {
-        device: {
-            name: '',
-            udid: '',
-            apps: '',
-        },
-    },
-    mounted: function () {
-        this.refresh();
-
-    },
-    methods: {
-        refresh: function () {
-            console.log("getting device info...");
-            var self = this;
-            $.ajax({
-                url: "/ios/getDeviceInfo?",
-                method: "GET",
-            })
-                .then(function (ret) {
-                    self.device.udid = ret.udid
-                    self.device.name = ret.name
-                    self.device.apps = ret.apps
-                    console.log(udid)
-                })
-        },
-
-    },
-});
 
 function refreshdetail() {
     console.log("getting device info...");
+    var self = this;
     $.ajax({
         url: "/ios/getDeviceInfo?",
         method: "GET",
     })
         .then(function (ret) {
-            deviceDetail.device.udid = ret.udid
-            deviceDetail.device.name = ret.name
-            deviceDetail.device.apps = ret.apps
+            self.device.udid = ret.udid
+            self.device.name = ret.name
+            self.device.apps = ret.apps
+            self.device.type = ret.type
+            self.device.version = ret.version
             console.log(udid)
+            var typeString = self.device.type
+            var realTypeName = iosinfo[typeString]["iPhoneType"]
+            console.log(realTypeName)
+            document.getElementById('deviceTypeName').textContent = realTypeName
+            imgX = iosinfo[typeString]["ptX"]
+            imgY = iosinfo[typeString]["ptY"]
         })
 }
